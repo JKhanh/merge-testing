1f6570b6e304a65931f712738e271badda6ece1a
Jonas Khanh <nguyenkhanhcv@gmail.com>
2019-05-24 20:26:34 +0700

    add feature print your age

--- src/com/k001/main/Main.java
+++ src/com/k001/main/Main.java
@@ -1,4 +1,12 @@
 package com.k001.main;
 
 public class Main {
+    public static void printAge(int age)
+    {
+        System.out.println("Tuoi toi la: " + age);
+    }
+
+    public static void main(String[] args) {
+        printAge("21");
+    }
 }
